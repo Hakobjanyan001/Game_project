437f58ad5aad759c86c2f3793d22cb90a467212f
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2026-03-19 15:59:51 +0400

    add mission 1 & mission 2

--- main.js
+++ main.js
@@ -4,46 +4,93 @@ const config = {
     height: 600,
     physics: {
         default: 'arcade',
-        arcade: { gravity: { y: 1000 } }
+        arcade: { gravity: { y: 0 } }
     },
     scene: {
         preload: preload,
         create: create,
-        update: update 
+        update: update
     }
 };
 
 let player;
+let orb;
 let cursors;
+
+// Arlo xax=i hamar
+let currentMission = 1;
+let missionText;
+let villageCenter;
+let missionStatus = "incomplete";
+
 const game = new Phaser.Game(config);
 
 function preload() {
     this.load.setBaseURL('https://labs.phaser.io');
+    
+    this.load.image('sky', 'assets/skies/space3.png');
     this.load.image('ball', 'assets/sprites/shinyball.png');
+    this.load.image('orb', 'assets/sprites/aqua_ball.png');
 }
 
 function create() {
-    // nerbernuma xaxacoxin ( gndaky )
-    player = this.physics.add.image(400, 300, 'ball');
-    player.setCollideWorldBounds(true); // tuyl chi tali dusga ekranic
+    this.add.image(400, 300, 'sky');
 
-    // stexcuma slaqnerin lsox sarq 
+    player = this.physics.add.image(100, 300, 'ball');
+    player.setCollideWorldBounds(true);
+
+    orb = this.physics.add.staticImage(700, 300, 'orb');
+    orb.setTint(0xff0000); // funkcia vory ognma nkari guyny
+
+    //Gexamej-y missia 2 
+    villageCenter = this.physics.add.staticImage(100,100, 'orb');
+    villageCenter.setTint(0xffff00);
+    villageCenter.setAlpha(0); // minchev missia 1-i avarty antesaneli
+
     cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-    // karavarelu uxxutyunnery
-    player.setVelocity(0);
+    player.setVelocity(0); // erb stexn chka sexmac kangni inerciai tak chgna
 
-    if (cursors.left.isDown) {
-        player.setVelocityX(-200);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(200);
+    if (cursors.left.isDown) player.setVelocityX(-200);
+    else if (cursors.right.isDown) player.setVelocityX(200);
+    
+    if (cursors.up.isDown) player.setVelocityY(-200);
+    else if (cursors.down.isDown) player.setVelocityY(200);
+
+    if(currentMission == 1) {
+        checkMission1();
+    } else if(currentMission == 2) {
+        checkMission2();
     }
 
-    if (cursors.up.isDown) {
-        player.setVelocityY(-200);
-    } else if (cursors.down.isDown) {
-        player.setVelocityY(200);
+    let distance = Phaser.Math.Distance.Between(player.x, player.y, orb.x, orb.y);
+
+    if(currentMission >= 2 && distance > 100) {
+        orb.setVisible(false); // erb heruana kori
+        orb.disableBody(true, true); // nev fizikapes kori baxumic ban charajana
+    } else {
+        orb.setVisible(true);
+    }
+}
+
+function checkMission1() {
+    let distance = Phaser.Math.Distance.Between(player.x, player.y, orb.x, orb.y);
+
+    if (distance < 20) {
+        currentMission = 2;
+        
+        orb.setTint(0x00ff00); 
+
+        villageCenter.setAlpha(1) //missa 1-i avarti verjum haytnvi missa 2-i skizby
+    }
+}
+
+function checkMission2() {
+    let distance = Phaser.Math.Distance.Between(player.x, player.y, villageCenter.x, villageCenter.y);
+    if(distance < 20) {
+        currentMission = 3; // hasav gexamej poxv ec 3-rd missa
+        villageCenter.setTint(0xffffff);
     }
 }
